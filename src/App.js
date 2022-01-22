193c294da02f379c31d4df62491cd520b0e8e8de
Adrian Ozuna <adriangabrielozuna133@zohomail.com>
2022-01-22 13:38:58 -0400

    Created some components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import logo from './img/logo.svg';
 import './css/App.css';
+import './sass/main.scss';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import StartPage from './StartPage';
+import AdminLogin from './AdminLogin';
+import Panel from './Panel';
+import NotFound from './NotFound';
+import Layout from './components/Layout';
+import NavigationBar from './components/NavigationBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <NavigationBar />
+      <Layout>
+        <Routes>
+          <Route exact path="/" element={<StartPage />} />
+          <Route path="/admin" element={<AdminLogin />} />
+          <Route path="/panel" element={<Panel />} />
+          <Route element={<NotFound />} />
+        </Routes>
+      </Layout>
+    </React.Fragment>
   );
 }
 
---
+++ src/components/NavigationBar.js
@@ -0,0 +1,35 @@
+import { Nav, Navbar, Containerfluid } from 'react-bootstrap';
+import styled from 'styled-components';
+import logo from '../assets/logo.svg'
+
+const Styles = styled.div`
+  .navbar {
+    background-color: #F23131;
+    height: 6.563rem;
+    padding-left: 1rem;
+  }
+
+  .navbar-brand {
+    color: #FFFF;
+  }
+`
+
+const NavigationBar = () => {
+  return (
+    <Styles>
+      <Navbar>
+        <Navbar.Brand>
+          <img
+            src={logo}
+            width="57"
+            height="57"
+            className="d-inline-block align-top"
+            alt="React Bootstrap logo"
+          />
+        </Navbar.Brand>
+      </Navbar>
+    </Styles>
+  )
+};
+
+export default NavigationBar;
