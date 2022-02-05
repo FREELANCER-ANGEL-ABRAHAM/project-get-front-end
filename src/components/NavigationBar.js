7b0ef048e67b90b9913272d0f3cdf0468788b1e5
Adrian Ozuna <adriangabrielozuna133@zohomail.com>
2022-02-05 19:04:54 -0400

    Improved navbar

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,12 +1,14 @@
 import { Navbar } from 'react-bootstrap';
 import styled from 'styled-components';
-import logo from '../assets/logo.svg'
+import logo from '../assets/logo.svg';
+
 
 const Styles = styled.div`
   .navbar {
     background-color: #F23131;
     height: 6.563rem;
-    padding-left: 1rem;
+    padding-left: 1.5rem;
+    padding-right: 1.5rem;
   }
 
   .navbar-brand {
@@ -14,19 +16,30 @@ const Styles = styled.div`
   }
 `
 
-const NavigationBar = () => {
+const NavigationBar = ({navbarTitle, brandDisplay, settingsDisplay}) => {
+
   return (
     <Styles>
       <Navbar>
         <Navbar.Brand>
-          <img
-            src={logo}
-            width="57"
-            height="57"
-            className="d-inline-block align-top"
-            alt="React Bootstrap logo"
-          />
+          <a href='/'>
+            <img
+              src={logo}
+              width="57"
+              height="57"
+              className={brandDisplay || "d-none"}
+              alt="React Bootstrap logo"
+            />
+          </a>
         </Navbar.Brand>
+        <h1 className='mx-auto text-light' style={{fontSize: "36px", fontWeight: "700"}}>{navbarTitle}</h1>
+        <Navbar.Text>
+          <span className={settingsDisplay}>
+            <a href='/settings'>
+              <i className='bi bi-gear-fill fa-lg text-light' style={{fontSize: "1.5em"}} ></i>
+            </a>
+          </span>
+        </Navbar.Text>
       </Navbar>
     </Styles>
   )
