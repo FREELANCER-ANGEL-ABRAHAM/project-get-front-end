26cfd0bfa0aebba5199e1b1f0861b5e1e9c0ea07
Adrian Ozuna <adriangabrielozuna133@zohomail.com>
2022-02-05 19:05:26 -0400

    Improved styled custom button

--- src/components/CustomButton.js
+++ src/components/CustomButton.js
@@ -1,22 +1,39 @@
 import styled from "styled-components";
 
-const Styles = styled.button`
-    border: none;
-    border-radius: 5px;
-    background-color: #C8000C;
-    color: #FFFF;
-    font-size: 2.25em;
-    font-weight: bold;
-    text-transform: uppercase;
-    padding: 1rem;
-`
-
-const CustomButton = ({ className, text }) => {
+const CustomButton = ({ className, children, onClick, type, fontSize }) => {
   return (
-    <Styles className={className}>
-        {text}
+    <Styles className="custom-button" onClick={onClick} type={type} fontSize={fontSize}>
+      {children}
     </Styles>
   );
 };
+
+const Styles = styled.button`
+
+  @media (max-width: 768px) {
+    font-size: 1.5rem;
+  }
+
+  @media (min-width: 772px) {
+    font-size: 1.5rem;
+  }
+
+  @media (min-width: 996px) {
+    font-size: 2.25rem;
+  }
+
+  border: none;
+  border-radius: 5px;
+  background-color: #F23131;
+  color: #FFFF;
+  font-weight: bold;
+  text-transform: uppercase;
+  padding: 1rem;
+  width: 100%;
+
+  &:hover {
+    background-color: #B01313;
+  }
+`
 
 export default CustomButton;
