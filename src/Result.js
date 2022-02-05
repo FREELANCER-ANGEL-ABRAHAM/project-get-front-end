52bdad1b0e04c1249d7520cfa11a25f871698d98
Adrian Ozuna <adriangabrielozuna133@zohomail.com>
2022-02-05 19:06:49 -0400

    Modified result page

--- src/Result.js
+++ src/Result.js
@@ -1,19 +1,14 @@
 import { Card, Row } from "react-bootstrap";
 import CustomIcon from "./components/CustomIcon";
-import logo from "./assets/logo.svg"
-import lock from "./assets/lock.svg"
-import SocialIcon from "./components/SocialIcon";
-import CustomButton from "./components/CustomButton";
+import success from "./assets/check.svg"
 
 function Result() {
   return (
     <Row className="m-2 m-md-0" style={{height: "80vh", overflow: "hidden"}}>
       <Card className="col-12 col-md-4 mx-auto my-auto d-flex shadow p-4 text-center" style={{border: "none"}}>
-        <CustomIcon src={lock} alt="icon" height={25}></CustomIcon>
-        <Card.Title style={{fontWeight: "bold", fontSize: "1.5em"}}>Listo.</Card.Title>
-        <Card.Body style={{fontSize: "1em"}}>Para acceder a este contenido realiza lo siguiente:</Card.Body>
-        <SocialIcon src={logo} height={50} alt="Social Media Icon" />
-        <CustomButton text="DALE LIKE"/>
+        <CustomIcon src={success} alt="icon" height={25}></CustomIcon>
+        <Card.Title style={{fontWeight: "bold", fontSize: "1.5em"}}>Haz completado todos los pasos..</Card.Title>
+        <Card.Body style={{fontSize: "1em"}}>Contenido: -</Card.Body>
       </Card>
     </Row>
   );
