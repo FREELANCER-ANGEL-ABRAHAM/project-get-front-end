971844db1d036cc02a0d7170194fc44563571082
Adrian Ozuna <adriangabrielozuna133@zohomail.com>
2022-02-05 19:07:24 -0400

    Added bootstrap icons

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import "bootstrap-icons/font/bootstrap-icons.css";                
 
 ReactDOM.render(
   <BrowserRouter>
