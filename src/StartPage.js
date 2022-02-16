50f78b040f8d06b509e1466b23fe6796fc5a3a03
Adrian Ozuna <adriangabrielozuna133@zohomail.com>
2022-02-15 20:22:49 -0400

    First commit

---
+++ src/StartPage.js
@@ -0,0 +1,41 @@
+import { Card, Row, Button } from "react-bootstrap";
+import CustomIcon from "./components/CustomIcon";
+import socialLogo from "./assets/socialmedia-logos/instagram.svg"
+import lock from "./assets/lock.svg"
+import SocialIcon from "./components/SocialIcon";
+import CustomButton from "./components/CustomButton";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+const StartPage = () => {
+
+  const navigate = useNavigate()
+
+  const [enableButton, setEnableButton] = useState(true)
+
+  return (
+    <div className="col-12 col-md-6 col-lg-4" style={{position: "absolute", top: "calc(45% - 105px - 123px)", margin: "0 auto", left:"0", right: "0"}}>
+      <div className="p-2">
+        <Card className="mx-auto shadow p-3 text-center" style={{border: "none",}}>
+            <CustomIcon src={lock} alt="icon" height={25}></CustomIcon>
+            <Card.Title style={{fontWeight: "bold", fontSize: "1.5em"}}>Complete los pasos para continuar.</Card.Title>
+            <Card.Body style={{fontSize: "1em"}}>Para acceder a este contenido realiza lo siguiente:</Card.Body>
+            <SocialIcon src={socialLogo} className="text-center mt-3 mb-5" height={80} width={82.05} alt="Social Media Icon" />
+            <CustomButton children="DALE LIKE" onClick={ () => setEnableButton(false)} style={{height: "4.063rem"}} />
+          </Card>
+      
+          <Button 
+            className="w-100 mx-auto btn-lg p-4 mt-2" 
+            variant="danger" 
+            onClick={() => navigate('/result')} 
+            style={{ border: "none"}} 
+            disabled={enableButton}>
+              Continuar
+          </Button>
+      </div>
+      
+    </div>
+  );
+};
+
+export default StartPage;
---
+++ src/components/CustomButton.js
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+const Styles = styled.button`
+
+  @media (max-width: 768px) {
+    font-size: 1.5rem;
+  }
+
+  @media (min-width: 772px) {
+    font-size: 1.5rem;
+  }
+
+  @media (min-width: 996px) {
+    font-size: 2.25rem;
+  }
+
+  border: none;
+  border-radius: 5px;
+  background-color: #F23131;
+  color: #FFFF;
+  font-weight: bold;
+  text-transform: uppercase;
+ 
+  width: 100%;
+
+  &:hover {
+    background-color: #B01313;
+  }
+`
+
+const CustomButton = ({ className, children, onClick, type, fontSize, style }) => {
+  return (
+    <Styles className="custom-button" onClick={onClick} type={type} fontSize={fontSize} style={style} >
+      {children}
+    </Styles>
+  );
+};
+
+export default CustomButton;
