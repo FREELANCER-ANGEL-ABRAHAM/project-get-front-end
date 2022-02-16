7e508d876d57bb72521e7fba4dbdf123c6c0fe79
Adrian Ozuna <87553186+StereoTurtle@users.noreply.github.com>
2022-02-15 20:03:42 -0400

    Revert "Homepage"

--- src/App.js
+++ src/App.js
@@ -1,68 +1,19 @@
+import logo from './img/logo.svg';
 import './css/App.css';
-import './sass/main.scss';
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, useLocation} from 'react-router-dom';
-import StartPage from './StartPage';
-import AdminLogin from './AdminLogin';
-import Panel from './Panel';
-import NotFound from './NotFound';
-import Layout from './components/Layout';
-import Settings from './Settings';
-import NavigationBar from './components/NavigationBar';
-import Result from './Result';
-import CreateLink from './CreateLink';
-import Footer from './components/Footer';
-import ModifyLink from './ModifyLink';
-import backArrow from './assets/backarrow.svg'
 
 function App() {
-
-  let location = useLocation()
-  let navbarComponent
-  let footerComponent
-
-  if(location.pathname === "/panel") {
-    navbarComponent = <NavigationBar navbarTitle="Mis links" brandDisplay="d-block" />
-    footerComponent = null
-  } else if(location.pathname === "/create") {
-    navbarComponent = <NavigationBar navbarTitle="Crear link" />
-  } else if(location.pathname === "/modify"){
-    navbarComponent = <NavigationBar navbarTitle="Modificar link" />
-  } else if(location.pathname === "/admin") {
-    footerComponent = <Footer actionText="Regresar" actionHref="/" />
-    navbarComponent = <NavigationBar brandDisplay="d-block" settingsDisplay="d-none"/>
-  } else if(location.pathname === "/settings"){
-    footerComponent = null
-    navbarComponent = <NavigationBar 
-                        brandDisplay="d-block" 
-                        settingsDisplay="d-none" 
-                        logo={backArrow} 
-                        logoWidth="57px" 
-                        logoHeight="57px" 
-                        href="/panel" />
-  } else {
-    navbarComponent = <NavigationBar brandDisplay="d-block" settingsDisplay="d-none"/>
-    footerComponent = <Footer />
-  }
-
   return (
-    <React.Fragment>
-      {navbarComponent}
-      <Layout>
-        <Routes>
-          <Route exact path="/" element={<StartPage />} />
-          <Route path="/admin" element={<AdminLogin />} />
-          <Route path="/panel" element={<Panel />} />
-          <Route path="/result" element={<Result />} />
-          <Route path="/create" element={<CreateLink />} />
-          <Route path="/modify:id" element={<ModifyLink />} />
-          <Route path="/settings" element={<Settings />} />
-          <Route path="*" exact="true" element={<NotFound />} />
-        </Routes>
-      </Layout>
-      {footerComponent}
-      
-    </React.Fragment>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
--- src/components/Image.js
+++
@@ -1,5 +0,0 @@
-function Image({ src, alt, height, width }) {
-  return <img src={src} alt={alt} height={height} width={width} />;
-}
-
-export default Image;
