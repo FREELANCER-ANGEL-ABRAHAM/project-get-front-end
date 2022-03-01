7ebb6b09856fd9d7ed14cec1881c2fb86f992063
Angel Abraham Nuñez Feliz <angelabrahannunez@gmail.com>
2022-03-01 04:12:57 -0400

    Se realizaron las sugerencias de github actions

--- src/components/LinkSearchModal.js
+++ src/components/LinkSearchModal.js
@@ -1,4 +1,4 @@
-import { React, useState, useEffect } from 'react';
+import { React, useState, useEffect, useCallback } from 'react';
 import { Button, Form, Row, Pagination } from 'react-bootstrap';
 import Modal from 'react-bootstrap/Modal'
 import AuthLinkProvider from '../service/authLink';
