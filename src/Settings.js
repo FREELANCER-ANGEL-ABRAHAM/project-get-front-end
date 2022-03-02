7d788ce6fec4e9682f65fb8d5d425e5dc6212957
Juan Capellan <juanluiscapellan1@gmail.com>
2022-03-02 08:24:16 -0400

    issue sonar

--- src/Settings.js
+++ src/Settings.js
@@ -7,7 +7,7 @@ import TokenService from './service/authToken';
 const Settings = ()  => {
   const navigate = useNavigate();
 
-  const [pictureValue, setPictureValue] = useState(null);
+  const [ , setPictureValue] = useState(null);
   const [errorMessage, setErrorMessage] = useState('');
   const [error, setError] = useState(false);
   const [imagePreview, setImagePreview] = useState(null);
