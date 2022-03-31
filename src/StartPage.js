d7af2cf1df603787f821b9ddbdd10db8ed7309fb
Juan Capellan <Juanluiscapellan1@gmail.com>
2022-03-31 00:19:33 -0400

    add loading to startpage

--- src/StartPage.js
+++ src/StartPage.js
@@ -8,12 +8,14 @@ import { useState, useEffect } from "react";
 import AuthLinkProvider from "./service/AuthLinkProvider";
 import TokenService from "./service/TokenService";
 import './sass/components/_button.scss';
+import Spinner from 'react-bootstrap/Spinner';
 
 const StartPage = (props) => {
 
   const navigate = useNavigate()
 
   const [enableButton, setEnableButton] = useState(true);
+  const [loading, setLoading] = useState(false);
   const { id } = useParams();
   const [links, setLinks] = useState({
     id: 1,
@@ -27,24 +29,27 @@ const StartPage = (props) => {
   useEffect(() => {
     ( async () => {
       try {
-        const response = await AuthLinkProvider.getLinkById(id);
-        if(response){
-          setLinks(response);
-          const currentStorage = JSON.parse(localStorage.getItem('Clicked'));
-          if(currentStorage === null){
-            setEnableButton(true);  
-          }
-          else{
-            let temp = JSON.parse(localStorage.getItem('Clicked'));
-            if(temp.id !== response._id){
-              localStorage.removeItem('Clicked');
+        setTimeout(async function () {
+          const response = await AuthLinkProvider.getLinkById(id);
+          if(response){
+            setLinks(response);
+            const currentStorage = JSON.parse(localStorage.getItem('Clicked'));
+            if(currentStorage === null){
+              setEnableButton(true);  
             }
             else{
-              setEnableButton(false);
+              let temp = JSON.parse(localStorage.getItem('Clicked'));
+              if(temp.id !== response._id){
+                localStorage.removeItem('Clicked');
+              }
+              else{
+                setEnableButton(false);
+              }
+              
             }
-            
           }
-        }
+          setLoading(true);
+        }, 1000);
       } catch (error) {
         console.error(error);
       }
@@ -73,12 +78,13 @@ const StartPage = (props) => {
 
   return (
     <div className="col-12 col-md-6 col-lg-4 mx-auto p-2" style={{minHeight: "calc(100vh - 7.688rem)", display: "flex", alignItems: "center"}}>
-      <Container>
-        <Card className="mx-auto shadow p-3 text-center" style={{border: "none",}}>
+      {loading ? (
+        <Container>
+          <Card className="mx-auto shadow p-3 text-center" style={{border: "none",}}>
             <CustomIcon src={lock} alt="icon" height={25}></CustomIcon>
             <Card.Title style={{fontWeight: "bold", fontSize: "1.5em"}}>{links.title}</Card.Title>
             <Card.Body style={{fontSize: "1em", padding: 0}}>{links.description}</Card.Body>
-            { links.image ? <SocialIcon src={links.image} className="text-center m-3" height={80} width={82.05}/> : null }
+             {links.image ? <SocialIcon src={links.image} className="text-center m-3" height={80} width={82.05}/> : null}
             <a href={path} target="_blank" rel="noopener noreferrer">
               <CustomButton children={links.btn_name} onClick={ () => routeChange()} style={{}} />
             </a>
@@ -92,8 +98,12 @@ const StartPage = (props) => {
             disabled={enableButton}>
               Continuar
           </Button>
-      </Container>
-      
+        </Container>
+      ) : (
+        <Spinner animation="border" role="status" className="text-center">
+          <span className="visually-hidden">Loading...</span>
+        </Spinner>
+      )}
     </div>
   );
 };
